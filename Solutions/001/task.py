2b25a49bb11cf1fcd45a00247310991ada59c23b
Michael Tinsley <michaeltinsley@outlook.com>
2020-12-28 16:38:14 +0000

    Task 1

--- Solutions/001/task.py
+++ Solutions/001/task.py
@@ -1,51 +1,38 @@
-from typing import Generator, List, Tuple
+from typing import List, Optional, Tuple
 
 
-def factor_generator(value: int) -> Generator[int, None, None]:
+def union_of_multiples(
+    multiples: Tuple[int], upperbound: int, lowerbound: int
+) -> List[int]:
     """
-    Finds all factors of a given value.
+    Returns the union of a set of multiples.
 
-    :param value: The upper
-    :return: A generator object
-    """
-    return (i for i in range(1, value + 1) if value % i == 0)
-
-
-def multiple_of(multiple: int, value: int) -> bool:
-    """
-
-    :param multiple:
-    :param value:
-    :return:
-    """
-    return value % multiple == 0
-
-
-def union_of_multiples(multiples: Tuple[int], upperbound: int) -> List[int]:
-    """
-
-
-    :param multiples:
-    :param upperbound:
-    :return:
+    :param multiples: Specified multiple values
+    :param upperbound: The upperbound limit
+    :param lowerbound: The lowerbound limit
+    :return: A set of the union of all multiples
     """
     all_factors = [
-        set(range(0, upperbound, multiple)) for multiple in multiples
+        set(range(lowerbound, upperbound, multiple)) for multiple in multiples
     ]
     return set().union(*all_factors)
 
 
-def sum_multiples(multiples: Tuple[int], upperbound: int) -> None:
+def sum_union(
+    multiples: Tuple[int], upperbound: int, lowerbound: Optional[int] = 0
+) -> None:
     """
+    Sums the union of multiples.
 
-    :param multiples:
-    :param upperbound:
-    :return:
+    :param multiples: Specified multiple values
+    :param upperbound: The upperbound limit
+    :param lowerbound: The lowerbound limit. Defaults to 0.
+    :return: The sum of the union of all multiples
     """
-    intersection = union_of_multiples(multiples, upperbound)
+    intersection = union_of_multiples(multiples, upperbound, lowerbound)
     return sum(intersection)
 
 
 if __name__ == "__main__":
-    print(sum_multiples(multiples=(3, 5), upperbound=10))
-    print(sum_multiples(multiples=(3, 5), upperbound=1000))
+    print(sum_union(multiples=(3, 5), upperbound=10))
+    print(sum_union(multiples=(3, 5), upperbound=1000))
